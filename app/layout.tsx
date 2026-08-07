395ae99c1c8195716a9293ae6151e2c9d7067367
f6669w8h67-hub <313327776+f6669w8h67-hub@users.noreply.github.com>
2026-08-07 20:10:59 +0800

    Add navbar styles import to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./pendulum-compact.css";
+import "./navbar.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
